ced01af15efb055769e1a61a325973c1fa4dbae8
Luigi Niso <luiginiso04@gmail.com>
2022-04-29 10:54:35 +0200

    Error index out of bound

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        
+        q.pushPerson(new Person("aaa", "Luigi", "Niso"));
+        q.pushPerson(new Person("bbb", "Maria", "Mastroleo"));
+        q.pushPerson(new Person("ccc", "Mario", "Rossi"));
+        q.pushPerson(new Person("ddd", "Giuseppe", "Verdi"));
+        q.pushPerson(new Person("eee", "Antonio", "Gialli"));
+
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+        
+        q.read();
+
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+
+        System.out.println(q.popPerson());
+
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+        
+        q.read();
+
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+
+        
+        q.pushPerson(new Person("fff", "Francesco", "LaGrotta"));
+        q.read();
+    }
+}
--- Queue.java
+++ Queue.java
@@ -1,3 +1,50 @@
 public class Queue {
-    private    
+    private final int MAX_PERSON = 5;
+    private Person list[] = new Person[MAX_PERSON];
+    private int push = 0;
+    private int pop = 0;
+    
+    public Queue(){}
+
+    public boolean pushPerson(Person p){
+        if(push > list.length){
+            if(pop == 0){
+                return false;
+            }else{
+                shift();
+                list[push] = p;
+                push++;
+                return true;
+            }
+        }else{
+            list[push] = p;
+            push++;
+            return true;
+        }
+    }
+
+    public void shift(){
+        for(int i = pop; i < list.length; i++){
+            for(int j = 0; j < list.length - pop; j++){
+                list[i] = list[j];
+            }
+        }
+        push = pop;
+    }
+
+    public Person popPerson(){
+        int k = pop;
+        pop++;
+        if(pop > list.length) pop=0;
+        return list[k];
+    }
+
+    public void read(){
+        int k = pop;
+        while(k != push){
+            System.out.println(list[k]);
+            k++;
+            if(k > list.length) k = 0;
+        }
+    }
 }
